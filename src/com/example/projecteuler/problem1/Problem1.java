b6bbea84c1c8585fccf44ab91c82807deeaace08
Pritish Shah <pritishshah.it@gmail.com>
2014-02-05 13:01:49 -0800

    first file

---
+++ src/com/example/projecteuler/problem1/Problem1.java
@@ -0,0 +1,13 @@
+package com.example.projecteuler.problem1;
+
+public class Problem1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
